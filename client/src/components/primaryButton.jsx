45c153128b7894d11758c81b39115fd7185a422f
Aditya Shukla <shukladitya9@gmail.com>
2021-09-20 23:58:25 +0530

    primary btn

--- client/src/components/primaryButton.jsx
+++ client/src/components/primaryButton.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-function PrimaryButton() {
-  return <div>Login</div>;
+function PrimaryButton(props) {
+  const style = {
+    backgroundImage: "linear-gradient(45deg, #7b1fa2, #e91e63)",
+    display: "inline-black",
+    paddingTop: "10px",
+    paddingBottom: "10px",
+    textAlign: "center",
+    width: props.width,
+    color: "white",
+    fontWeight: "bold",
+    cursor: "pointer",
+  };
+  return <div style={style}>{props.text}</div>;
 }
 
-export default PrimaryButton
+export default PrimaryButton;
--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -5,14 +5,25 @@ import SecondaryButton from "../components/secondaryButton";
 import { Link } from "react-router-dom";
 
 function Login() {
+  const bannerStyle = {
+    backgroundColor: "black",
+    backgroundImage: "url('https://source.unsplash.com/user/adity_a/likes/')",
+    height: "100vh",
+    flexGrow: "100",
+    transition: "all 1s",
+  };
   return (
-    <div>
-      <TextField />
-      <TextField />
-      <PrimaryButton />
-      <Link to="/signup">
-        <SecondaryButton />
-      </Link>
+    <div style={{ display: "flex" }}>
+      <div style={bannerStyle}></div>
+      <div style={{ padding: "20px" }}>
+        <h2>JusAsk</h2>
+        <TextField />
+        <TextField />
+        <PrimaryButton text="Login" width="160px" />
+        <Link to="/signup">
+          <SecondaryButton />
+        </Link>
+      </div>
     </div>
   );
 }
